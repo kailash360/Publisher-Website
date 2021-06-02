d693e0d81264cc40eed25d0d26bc210cac64c576
kailash360 <kailashkejriwal21@gmail.com>
2021-06-02 19:29:37 +0530

    fixed card size

--- src/components/homepage/Card.js
+++ src/components/homepage/Card.js
@@ -4,20 +4,20 @@ import '../../static/css/card.css'
 const Card_item = ({ name, description, image, link }) => {
     return (
         <>
-            <a href={link}>
-                <div className='card mb-3'>
+            <div className='card mb-3'>
+                <a href={link}>
                     <div className="row g-0">
-                        <div className="col-md-3">
+                        <div className="col-md-4">
                             <img class="card-image" src={image} alt="Image" />
                         </div>
-                        <div className="col-md-8 text-container">
+                        <div className="col-md-8">
                             <div className="card-body">
                                 <h2 className="card-title"><a href={link}>{name}</a></h2>
                                 <p className="card-text"><a href={link}>{description}</a></p>
                             </div>
                         </div>
                     </div>
-                </div></a>
+                </a></div>
         </>
     );
 }
